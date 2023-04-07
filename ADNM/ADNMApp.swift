3255ac8d050e8d37a9504d8cbc2466c602336917
谢渊 <xieyuan@agree.com.cn>
2023-04-07 14:59:59 +0800

    Initial Commit

---
+++ ADNM/ADNMApp.swift
@@ -0,0 +1,17 @@
+//
+//  ADNMApp.swift
+//  ADNM
+//
+//  Created by 谢渊 on 2023/4/7.
+//
+
+import SwiftUI
+
+@main
+struct ADNMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
